34d6e46abaab0857ef7f8d745bda584d73b33f9c
Yann BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2023-04-24 14:54:48 +0200

    Added check in case .venv or .git already exist

---
+++ task.py
@@ -0,0 +1,38 @@
+import subprocess
+from typing import List
+from pathlib import Path
+
+
+def run_command(cmd: List[str], check: bool = True):
+    out = subprocess.run(cmd, stdout=subprocess.PIPE, check=check)
+    print(out.stdout)
+
+
+def initialize_git():
+    git_dir = Path(".git")
+    if not git_dir.exists():
+        run_command(["git", "init"])
+    run_command(["git", "add", "--all"])
+    run_command(["git", "commit", "-am", "Initial commit"])
+
+
+def update_venv():
+    venv_dir = Path(".venv")
+    if not venv_dir.exists():
+        run_command(["pdm", "venv", "create"])
+        run_command(["pdm", "use", "--venv", "in-project"])
+    run_command(["pdm", "install"], check=False)
+
+
+def update_commit_hooks():
+    run_command([".venv/bin/pre-commit", "install"])
+
+
+def main():
+    initialize_git()
+    update_venv()
+    update_commit_hooks()
+
+
+if __name__ == "__main__":
+    main()

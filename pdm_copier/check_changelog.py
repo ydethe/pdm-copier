f9bf237c272ded5ff8b4fcbf23bfc508aa0ad9bf
Yann BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2023-04-26 14:16:05 +0200

    Added stub arguments for filenames list

--- pdm_copier/check_changelog.py
+++ pdm_copier/check_changelog.py
@@ -30,6 +30,7 @@ def main(args: list[str] | None = None) -> int:
 
     """
     parser = get_parser()
+    parser.add_argument('filenames', nargs='*')
     to_remove: List[Action] = []
     name_to_remove = ["repository", "output", "version", "help"]
     for action in parser._actions:
